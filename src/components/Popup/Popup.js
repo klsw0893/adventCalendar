aa372759539ab87e1ce79a65bf9b012cc1b196f3
Király Lajos Szabolcs <kiraly.lajos.software@gmail.com>
2025-11-26 16:33:04 +0100

    popup content conditional render and set to the center

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -6,45 +6,49 @@ function Popup({ content, onClose }) {
     <div className={styles.overlay}>
       <div className={styles.popup}>
         <button className={styles.closeBtn} onClick={onClose}>X</button>
-        <div className={styles.content}>Advent {content.day}. napja</div>
-        <div className={styles.contentText}>
-          {
-            content.contentText && (<i>{content.contentText}</i>)
-          }
+        <div className={`${styles.content} ${content.type === "text" ? styles.centered : ''}`}>
+          {content.contentText}
         </div>
-        <div>
-          {
-            content.embeddedQuiz && (<iframe src={content.embeddedQuiz} width="100%" height="85%" frameborder="0" allowfullscreen></iframe>)
-          }
-        </div>
-        <div className={styles.contentText}>
-          {
-            content.embeddedLink.link && (
-              <a href={content.embeddedLink.link} target="_blank" rel="noopener noreferrer">
-                <img src={content.embeddedLink.pic} className={styles.videoPic} alt="Embedded link picture" />
-              </a>
+        {content.type !== "text" && (
+          <>
+            <div>
+              {
+                content.embeddedQuiz && (<iframe src={content.embeddedQuiz} width="90%" height="100%" frameborder="0" allowfullscreen></iframe>)
+              }
+            </div>
+            {content.embeddedLink.link && (<div className={styles.contentText}>
+              
+                
+                  <a href={content.embeddedLink.link} target="_blank" rel="noopener noreferrer">
+                    <img src={content.embeddedLink.pic} className={styles.videoPic} alt="Embedded link picture" />
+                  </a>
+                
+              
+            </div>
             )
-          }
-        </div>
-        <div className={styles.filmPic}>
-          {(content.video.netflixLink || content.video.otherVidLink) && (<img src={Film} className={styles.filmPic} alt="Film picture" />)}
-        </div>
-        <div className={styles.contentText}>
-          {
-            content.video.netflixLink && (
-              <a href={content.video.netflixLink} target="_blank" rel="noopener noreferrer">
-                <img src={content.video.netflixPic} className={styles.videoPic} alt="Netflix logo" />
-              </a>
-            )
-          }
-          {
-            content.video.otherVidLink && (
-              <a href={content.video.otherVidLink} target="_blank" rel="noopener noreferrer">
-                <img src={content.video.otherVidPic} className={styles.videoPic} alt="Videa logo" />
-              </a>
-            )
-          }
-        </div>
+            }
+            {(content.video.netflixLink || content.video.otherVidLink) && (
+            <div className={styles.filmPic}>
+              <img src={Film} className={styles.filmPic} alt="Film picture" />
+            </div>
+            )}
+            <div className={styles.contentText}>
+              {
+                content.video.netflixLink && (
+                  <a href={content.video.netflixLink} target="_blank" rel="noopener noreferrer">
+                    <img src={content.video.netflixPic} className={styles.videoPic} alt="Netflix logo" />
+                  </a>
+                )
+              }
+              {
+                content.video.otherVidLink && (
+                  <a href={content.video.otherVidLink} target="_blank" rel="noopener noreferrer">
+                    <img src={content.video.otherVidPic} className={styles.videoPic} alt="Videa logo" />
+                  </a>
+                )
+              }
+            </div>
+          </>)}
       </div>
     </div>
   );
